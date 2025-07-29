24adae994bc01386c8f339dac229d7ad1565105f
Alberto Cesar <albertocesar.be@gmail.com>
2025-07-28 22:14:34 -0300

    feat: func to get local ip

---
+++ utils.py
@@ -0,0 +1,11 @@
+import socket
+
+def get_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip_local = s.getsockname()[0]
+        s.close()
+        return ip_local
+    except Exception as e:
+        return f"Erro ao obter IP: {e}"
