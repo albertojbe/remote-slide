e38eb025b389ceaabc1d0f4287e65c1e9200b9a4
Alberto Cesar <albertocesar.be@gmail.com>
2025-07-28 22:15:00 -0300

    feat: create app on flask

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-from flask
+from flask import Flask
+from flask.templating import render_template
+import pyautogui
+
+import utils
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    ip = utils.get_ip()
+    print(ip)
+    return render_template('index.html', ip=ip)
+
+@app.route("/button/<key>")
+def pressKey(key: str):
+    print(key)
+    pyautogui.hotkey(key)
+    return {'status': 'ok'}
